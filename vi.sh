a39bddec30fcc5e66e80297e4a3317183d98395d
Barsbold Bayar-Erdene <barsbold.coding@gmail.com>
2024-03-02 21:00:50 +0800

    improvement on vi command

--- vi.sh
+++ vi.sh
@@ -2,22 +2,37 @@
 
 SEARCH_PATH=""
 
-if [ $1 ]
+if [ -n "$1" ]
 then
   SEARCH_PATH="*${1}*"
 else
   SEARCH_PATH="*"
 fi
 
-RES=""
+search_command() {
+  find * \
+    -path "*/node_modules" -prune -o \
+    -path "*/.git" -prune -o \
+    -path "$SEARCH_PATH" -type f
+}
+fzf_command() {
+  fzf +m --cycle --reverse \
+    --bind=tab:down,btab:up \
+    --preview 'bat --style=numbers --color=always --line-range :500 {}'
+}
 
-if [[ $(find . -path $SEARCH_PATH -type f | wc -l) -gt 1 ]]; then
-  RES=$(find . -path $SEARCH_PATH -type f | fzf)
+RES=""
+FC=$(search_command | wc -l)
+
+if [[ $FC -eq 0 ]]; then
+  echo "there is no matching file"
+elif [[ $FC -eq 1 ]]; then
+  RES=$(search_command)
 else
-  RES=$(find . -path $SEARCH_PATH -type f)
+  RES=$(search_command | fzf_command)
 fi
 
-if [ RES ]
+if [ -n "$RES" ]
 then
   nvim $RES
 fi
