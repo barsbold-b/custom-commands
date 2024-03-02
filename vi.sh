6177e56813140f7c0aacfd5559647dcf41da80b8
Barsbold Bayar-Erdene <barsbold.coding@gmail.com>
2024-03-02 18:49:50 +0800

    added vi command which open file with nvim quickly
    by using fzf and find commands, open file very quickly with nvim

---
+++ vi.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SEARCH_PATH=""
+
+if [ $1 ]
+then
+  SEARCH_PATH="${1}"
+else
+  SEARCH_PATH="*"
+fi
+
+RES=""
+
+if [[ $(find . -path "*${SEARCH_PATH}*" -type f | wc -l) -gt 1 ]]; then
+  RES=$(find . -path "*${SEARCH_PATH}*" -type f | fzf)
+else
+  RES=$(find . -path "*${SEARCH_PATH}*" -type f)
+fi
+
+if [ RES ]
+then
+  nvim $RES
+fi
