4afd1af38afacdfe6efc3f8011a058ba25a601e3
Barsbold Bayar-Erdene <barsbold.coding@gmail.com>
2024-03-02 18:52:16 +0800

    Update vi.sh with dry

--- vi.sh
+++ vi.sh
@@ -4,17 +4,17 @@ SEARCH_PATH=""
 
 if [ $1 ]
 then
-  SEARCH_PATH="${1}"
+  SEARCH_PATH="*${1}*"
 else
   SEARCH_PATH="*"
 fi
 
 RES=""
 
-if [[ $(find . -path "*${SEARCH_PATH}*" -type f | wc -l) -gt 1 ]]; then
-  RES=$(find . -path "*${SEARCH_PATH}*" -type f | fzf)
+if [[ $(find . -path $SEARCH_PATH -type f | wc -l) -gt 1 ]]; then
+  RES=$(find . -path $SEARCH_PATH -type f | fzf)
 else
-  RES=$(find . -path "*${SEARCH_PATH}*" -type f)
+  RES=$(find . -path $SEARCH_PATH -type f)
 fi
 
 if [ RES ]
